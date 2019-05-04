2f3387ab4ff8e956c0ff7cb073d481aa95f2a273
kota.kiguchi <kiguchikouta0320@gmail.com>
2019-05-04 07:07:45 +0000

    kadai-tasklist_v2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
     validates :content, presence: { message: 'タスクが空です' }, length: { maximum:255 , message: 'タスクは255文字以内にしてください' }
-    validates :status, presence: { message: 'タスクが空です' }, length: { maximum:10 , message: 'タスクは255文字以内にしてください' }
+    validates :status, presence: { message: 'ステータスが空です' }, length: { maximum:10 , message: 'ステータスは10文字以内にしてください' }
 end
