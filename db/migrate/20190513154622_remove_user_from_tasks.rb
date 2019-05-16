9add30ea6a6f2536747dd1d727ca592f277bb72b
kota.kiguchi <kiguchikouta0320@gmail.com>
2019-05-16 10:38:31 +0000

    task+user_v2

--- db/migrate/20190513154622_remove_user_from_tasks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromTasks < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tasks, :user_id, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_135211) do
+ActiveRecord::Schema.define(version: 2019_05_16_101519) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -29,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_05_14_135211) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
